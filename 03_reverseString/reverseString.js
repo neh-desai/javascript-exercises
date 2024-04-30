fdfbd2cd05003d104a255bf36e9a8354301a16cf
neh-desai <neh.desai2003@gmail.com>
2024-04-30 13:38:43 -0400

    Fix up cases 3 and 4
    Test suite passed for the JS code written

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let inputReverse = '';
+    for (let i = input.length-1; i>=0; i--){
+        inputReverse += input.charAt(i);
+    }
+    return inputReverse;
 };
 
 // Do not edit below this line
