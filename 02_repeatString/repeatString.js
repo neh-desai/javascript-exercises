f9b667e56a97f86ff6fd89050c5d4329bdb4cb77
neh-desai <neh.desai2003@gmail.com>
2024-04-29 22:38:29 -0400

    Solved introductory cases 1 and 2
    Figured out how to test my code via the preconfigured enviorment

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let final = '';
+    if (times <0){
+        return "ERROR";
+    }
+    else{
+        for (let i = 1; i<= times; i++){
+            final += word;
+        }
+        return final;
+    }
 };
 
 // Do not edit below this line
