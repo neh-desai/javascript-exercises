fa2955095dd9f91f960828de5a0fe1bc01b1d5cd
neh-desai <neh.desai2003@gmail.com>
2024-05-04 12:56:01 -0400

    Adjust case 4 and solve case 5 and 6
    Removed extra comments in case 4 and added the conditions to case 5 and 6 for the test suite to pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,14 @@
 const removeFromArray = function(arr, ...items) {
     let arrCopy = Array.from(arr);
-    // console.log(arrCopy);
-    // console.log(items);
-
+    
     for (let i = 0; i<items.length; i++){
         for (let j = 0; j<arr.length; j++){
-            //console.log(`Comparing ${items[i]} with ${arr[j]}`); 
             if (items[i] === arr[j]){
-                console.log("GOT HERE")
                 arrCopy = arr.splice(j,1);
                 j -=1;
             }
         }
     }
-    // console.log(arr)
     return arr;
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0; 
+    let temp = start;
+    if (start > end){
+        start = end
+        end = temp;
+    }
+    else if (start<0 || end<0){
+        return "ERROR";
+    }
+    else if (typeof arguments[0]!== "number" || typeof arguments[1]!== "number"){
+        return "ERROR"
+    }
+    
+    for (let i = start; i<=end; i++){
+        sum += i;
+    }
+    return sum; 
 };
-
 // Do not edit below this line
 module.exports = sumAll;
