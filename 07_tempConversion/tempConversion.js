ab1252463da156f8fa50611dc0d9dfc70bd4de7d
neh-desai <neh.desai2003@gmail.com>
2024-05-04 18:40:39 -0400

    Fix case 7
    Added code that passed the test suite of case 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+    let fahrenheit = ((number-32) * 5)/9;
+    fahrenheit = fahrenheit.toFixed(1);
+    return parseFloat(fahrenheit);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let celsius = ((number * 9)/5) + 32;
+  celsius = celsius.toFixed(1);
+  return parseFloat(celsius);
 };
 
 // Do not edit below this line
